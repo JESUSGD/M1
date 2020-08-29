4990ee56cfd82fdbbf4425bbb78a6940f96e3ebb
JESUSGD <66762744+JESUSGD@users.noreply.github.com>
2020-08-28 23:44:35 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.item = '';
+        $scope.message = 'Write items';
+
+        $scope.check = function () {
+            const words = $scope.item.split(' ');
+
+            console.log(words.length);
+            for (let i = 0; i < words.length; i++) {
+                if (words[i] == '' || words[i] == null) {
+                    return $scope.message = 'Empty item detected!';
+                } else if (words.length <= 3) {
+                    return $scope.message = 'Enjoy!';
+                } else if (words.length > 3) {
+                    return $scope.message = 'Too Much!';
+                }
+            }
+        }
+    }
+
+})();
